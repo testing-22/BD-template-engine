12c719f282d983e54dc43ad1229fd4c16d6930f6
juanbomfim22 <juanbomfim22@gmail.com>
2021-10-20 20:58:24 -0300

    First commit

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
+const products = require('./data/products')
 const express = require('express')
 const path = require('path')
 const app = express()
 
+
+// middlewares
 app.set('views', path.join(__dirname, 'views'))
 app.set('template engine', 'ejs')
-
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { products })
 })
 
 app.get('/:id', (req, res) => {
-    // console.log(req.params)
-    const items = {
-        camisa: {
-            id: 1,
-            name: 'Camisa',
-            category: 'Roupa',
-            amount: 10,
-            media: 15.56,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates dolorem aliquid quibusdam dolor suscipit deserunt mollitia! Animi error rem maxime aperiam dolores odio minus assumenda, asperiores suscipit ducimus aliquid. Assumenda repellendus quam veniam? Atque in omnis cum perspiciatis id hic aut reiciendis, esse illum corrupti nostrum maiores, temporibus quae cupiditate blanditiis doloribus eum sed! Molestiae perspiciatis totam sapiente cum.'
-        },
-        notebook: {
-            id: 1283,
-            name: 'Notebook',
-            category: 'Tecnologia',
-            amount: 25,
-            media: 1300.56,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates dolorem aliquid quibusdam dolor suscipit deserunt mollitia! Animi error rem maxime aperiam dolores odio minus assumenda, asperiores suscipit ducimus aliquid. Assumenda repellendus quam veniam? Atque in omnis cum perspiciatis id hic aut reiciendis, esse illum corrupti nostrum maiores, temporibus quae cupiditate blanditiis doloribus eum sed! Molestiae perspiciatis totam sapiente cum.'
-        }
-    }
-    res.render('product.ejs', { produto: })
-    if (req.params.id !== '1') res.render('product.ejs', { produto: notebook })
-    else res.render('product.ejs', { produto: camisa })
+    const pid = +req.params.id // string para int
+    const id = pid >= 1 && pid <= 20 ? pid : 1 // id padrão é 1
+    const product = products.filter(x => x.id === id )[0]// se nao houver id vai para o primeiro
+    res.render('product.ejs', { product })
 })
 
 const port = process.env.PORT || 3000
